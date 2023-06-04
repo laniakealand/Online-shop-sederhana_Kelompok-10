9337016603b3002535ccd1e0621fdef40fbb762a
Katon Adiluhung <katonadl27@student.uns.ac.id>
2023-06-04 12:24:45 +0700

    Menambahkan list lagu pop

---
+++ list lagu pop.py
@@ -0,0 +1,130 @@
+Pop =[
+    {'artis': 'Tiara Andini', 'judul': 'Gemintang Hatiku', 'tahun': '2021', 'link' : 'https://youtu.be/qQtPAggFMt8'}
+    {'artis': 'Tiara Andini', 'judul': 'Tega', 'tahun': '2023', 'link' : 'https://youtu.be/bLcRdYuS5xw'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Merasa Indah', 'tahun': '2022', 'link' : 'https://youtu.be/UHTVQVuVZTY'}
+    {'artis': 'Tiara Andini', 'judul': 'Maafkan Aku', 'tahun': '2021', 'link' : 'https://youtu.be/bxEmsKYADl8'}
+    {'artis': 'Tiara Andini', 'judul': 'Usai', 'tahun': '2023', 'link' : 'https://youtu.be/83ntBBDC8Xs'}
+    {'artis': 'Tiara Andini', 'judul': 'Menjadi Dia', 'tahun': '2022', 'link' : 'https://youtu.be/8qoL-fhPfO8'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Ego', 'tahun': '2023', 'link' : 'https://youtu.be/vjYJrDDCOf0'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Gemintang Hatiku', 'tahun': '2020', 'link' : 'https://youtu.be/H_x8_LXwTW4'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Pesan Terakhir', 'tahun': '2022', 'link' : 'https://youtu.be/oaFj0lRi3gw'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Mengapa Kita', 'tahun': '2021', 'link' : 'https://youtu.be/Mc9WSGhPshE'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Tentang Kamu', 'tahun': '2021', 'link' : 'https://youtu.be/w3xBNrKJGQo'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Kalau Bosan', 'tahun': '2022', 'link' : 'https://youtu.be/rMePfW-C7bc'}
+    {'artis': 'Lyodra Ginting', 'judul': 'Sabda Rindu', 'tahun': '2021', 'link' : 'https://youtu.be/o621gGw3dSU'}
+    {'artis': 'Ziva Magnolya', 'judul': 'Pilihan Yang Terbaik', 'tahun': '2023', 'link' : 'https://youtu.be/0g9b7Mgm3oo'}
+    {'artis': 'Ziva Magnolya', 'judul': 'Tak Sanggup Melupa', 'tahun': '2021', 'link' : 'https://youtu.be/BuU5dZb9W7o'}
+    {'artis': 'Mahalini', 'judul': 'Sial', 'tahun': '2023', 'link' : 'https://youtu.be/QSWYyoF79oE'}
+    {'artis': 'Mahalini', 'judul': 'Sisa Rasa', 'tahun': '2022', 'link' : 'https://youtu.be/Wh66ThpxvI4'}
+    {'artis': 'Mahalini', 'judul': 'Kisah Sempurna', 'tahun': '2022', 'link' : 'https://youtu.be/8dgEh5crj0I'}
+    {'artis': 'Mahalini', 'judul': 'Melawan Restu', 'tahun': '2021', 'link' : 'https://youtu.be/Svz5F8J1Ap0'}
+    {'artis': 'Mahalini', 'judul': 'Bohongi Hati', 'tahun': '2023', 'link' : 'https://youtu.be/weG-sqHHCB8'}
+    {'artis': 'Raim Laode', 'judul': 'Komang', 'tahun': '2023', 'link' : 'https://youtu.be/fKRtnMYMW08'}
+    {'artis': 'Aruma', 'judul': 'Muak', 'tahun': '2023', 'link' : 'https://youtu.be/6r5Bj_7VPwE'}
+    {'artis': 'Idgitaf', 'judul': 'Takut', 'tahun': '2022', 'link' : 'https://youtu.be/VJ1wfhlyWBw'}
+    {'artis': 'Anggi Marito', 'judul': 'Tak Segampang Itu', 'tahun': '2023', 'link' : 'https://youtu.be/6NsiA6GFAbU'}
+    {'artis': 'Once Mekel', 'judul': 'Symphony Yang Indah', 'tahun': '2016', 'link' : 'https://youtu.be/IzsOi_3TzM8'}
+    {'artis': 'Payung Teduh', 'judul': 'Akad', 'tahun': '2018', 'link' : 'https://youtu.be/viW0M5R2BLo'}
+    {'artis': 'JAZ', 'judul': 'Kasmaran', 'tahun': '2017', 'link' : 'https://youtu.be/4q24Qpn52CA'}
+    {'artis': 'JAZ', 'judul': 'Teman Bahagia', 'tahun': '2018', 'link' : 'https://youtu.be/1XTLb0_6JDs'}
+    {'artis': 'JAZ', 'judul': 'Dari Mata', 'tahun': '2016', 'link' : 'https://youtu.be/jieBCHsHg3A'}
+    {'artis': 'GAC', 'judul': 'Bahagia', 'tahun': '2015', 'link' : 'https://youtu.be/zDHOW9PdQYE'}
+    {'artis': 'GAC', 'judul': 'Cinta', 'tahun': '2016', 'link' : 'https://youtu.be/bkgfsvJvMSs'}
+    {'artis': 'HIVI', 'judul': 'Remaja', 'tahun': '2018', 'link' : 'https://youtu.be/tUJAxxm1y1I'}
+    {'artis': 'HIVI', 'judul': 'Pelangi', 'tahun': '2017', 'link' : 'https://youtu.be/bBjENsfyvFs'}
+    {'artis': 'VIERRA', 'judul': 'Rasa Ini', 'tahun': '2010', 'link' : 'https://youtu.be/RpC8NVgIfnc'}
+    {'artis': 'VIERRA', 'judul': 'Seandainya', 'tahun': '2011', 'link' : 'https://youtu.be/MofaZRiwiOw'}
+    {'artis': 'VIERRA', 'judul': 'Kesepian', 'tahun': '2012', 'link' : 'https://youtu.be/ZNVQaiyv6zU'}
+    {'artis': 'VIERRA', 'judul': 'Jadi Yang Kuinginkan', 'tahun': '2010', 'link' : 'https://youtu.be/4ofhlicIqD8'}
+    {'artis': 'VIERRA', 'judul': 'Terlalu Lama', 'tahun': '2011', 'link' : 'https://youtu.be/NgQVLbD-lQ0'}
+    {'artis': 'VIERRA', 'judul': 'Perih', 'tahun': '2010', 'link' : 'https://youtu.be/PqGmQiPQ0Tw'}
+    {'artis': 'VIERRA', 'judul': 'Bersamamu', 'tahun': '2010', 'link' : 'https://youtu.be/9VFip4CQT8I'}
+    {'artis': 'Sheila On 7', 'judul': 'Hari Bersamanya', 'tahun': '2011', 'link' : 'https://youtu.be/32M1DDVwqeY'}
+    {'artis': 'Sheila On 7', 'judul': 'Dan', 'tahun': '2013', 'link' : 'https://youtu.be/dGcGbF4ex5o'}
+    {'artis': 'Sheila On 7', 'judul': 'Seberapa Pantas', 'tahun': '2013', 'link' : 'https://youtu.be/OdDwIwoiZwU'}
+    {'artis': 'Sheila On 7', 'judul': 'Film Favorit', 'tahun': '2018', 'link' : 'https://youtu.be/cHKZ7eWiZjM'}
+    {'artis': 'Sheila On 7', 'judul': 'Melompat Lebih Tinggi', 'tahun': '2013', 'link' : 'https://youtu.be/ab9LL48USIU'}
+    {'artis': 'Sheila On 7', 'judul': 'Pemuja Rahasia', 'tahun': '2013', 'link' : 'https://youtu.be/HvSW5Kap81o'}
+    {'artis': 'Sheila On 7', 'judul': 'Sebuah Kisah Klasik', 'tahun': '2013', 'link' : 'https://youtu.be/WKsoUFblnfc'}
+    {'artis': 'Dmasiv', 'judul': 'Cinta Ini Membunuhku', 'tahun': '2016', 'link' : 'https://youtu.be/iz7hDDlW8yQ'}
+    {'artis': 'Dmasiv', 'judul': 'Diantara Kalian', 'tahun': '2009', 'link' : 'https://youtu.be/cQrSyOvRMzg'}
+    {'artis': 'Dmasiv', 'judul': 'Merindukanmu', 'tahun': '2009', 'link' : 'https://youtu.be/e86Whn8TaMg'}
+    {'artis': 'Dmasiv', 'judul': 'Cinta Sampai Disini', 'tahun': '2009', 'link' : 'https://youtu.be/NU2g0gXzkD0'}
+    {'artis': 'Dmasiv', 'judul': 'Apa Salahku', 'tahun': '2011', 'link' : 'https://youtu.be/GuGgrnau8BA'}
+    {'artis': 'Rossa', 'judul': 'Hijrah Cinta', 'tahun': '2014', 'link' : 'https://youtu.be/2DbHkjX-C1E'}
+    {'artis': 'Rossa', 'judul': 'Aku Bukan Untukmu', 'tahun': '2012', 'link' : 'https://youtu.be/OK-DNdo5VWE'}
+    {'artis': 'Rossa', 'judul': 'Tak Sanggup Lagi', 'tahun': '2013', 'link' : 'https://youtu.be/ALBIH1Z1W9Y'}
+    {'artis': 'Rossa', 'judul': 'Pudar', 'tahun': '2012', 'link' : 'https://youtu.be/1Ny9z6cHTZc'}
+    {'artis': 'Rossa', 'judul': 'Terlalu Cinta', 'tahun': '2012', 'link' : 'https://youtu.be/i3CYY6MSHIY'}
+    {'artis': 'Rossa', 'judul': 'Takdir Cinta', 'tahun': '2012', 'link' : 'https://youtu.be/Jhdn3kyCUEQ'}
+    {'artis': 'Rossa', 'judul': 'Hati Yang Kau Sakiti', 'tahun': '2012', 'link' : 'https://youtu.be/vY9vsQra-Kg'}
+    {'artis': 'Rossa', 'judul': 'Jangan Hilangkan Dia', 'tahun': '2017', 'link' : 'https://youtu.be/wfDwfZblwVo'}
+    {'artis': 'Rossa', 'judul': 'Ayat Ayat Cinta', 'tahun': '2012', 'link' : 'https://youtu.be/2zigf4LhZ1g'}
+    {'artis': 'Rossa', 'judul': 'Tega', 'tahun': '2012', 'link' : 'https://youtu.be/pOVFWMR6tAM'}
+    {'artis': 'Rossa', 'judul': 'Tegar', 'tahun': '2017', 'link' : 'https://youtu.be/dhy-mYfBN7Q'}
+    {'artis': 'Tulus', 'judul': 'Nala', 'tahun': '2023', 'link' : 'https://youtu.be/7wGB30ijgqQ'}
+    {'artis': 'Tulus', 'judul': 'Hati Hati Di Jalan', 'tahun': '2022', 'link' : 'https://youtu.be/9II3OGZETo4'}
+    {'artis': 'Tulus', 'judul': 'Tujuh Belas', 'tahun': '2022', 'link' : 'https://youtu.be/cA7Gzh2ISL0'}
+    {'artis': 'Tulus', 'judul': 'Monokrom', 'tahun': '2017', 'link' : 'https://youtu.be/QqJ-Vp8mvbk'}
+    {'artis': 'Tulus', 'judul': 'Pamit', 'tahun': '2016', 'link' : 'https://youtu.be/G2-ZC9NpDBg'}
+    {'artis': 'Tulus', 'judul': 'Teman Hidup', 'tahun': '2013', 'link' : 'https://youtu.be/dt4Ueda_h6Y'}
+    {'artis': 'Tulus', 'judul': 'Jangan Cintai Aku Apa Adanya', 'tahun': '2015', 'link' : 'https://youtu.be/5L1RVCtL1D0'}
+    {'artis': 'Tulus', 'judul': 'Sewindu', 'tahun': '2012', 'link' : 'https://youtu.be/wpst_4m_c-E'}
+    {'artis': 'Tulus', 'judul': 'Gajah', 'tahun': '2015', 'link' : 'https://youtu.be/I-el8UadDc4'}
+    {'artis': 'Tulus', 'judul': 'Sepatu', 'tahun': '2014', 'link' : 'https://youtu.be/DYbSp-B-8AM'}
+    {'artis': 'Tulus', 'judul': 'Manusia Kuat', 'tahun': '2017', 'link' : 'https://youtu.be/f-fEeF12FH4'}
+    {'artis': 'Tulus', 'judul': 'Ruang Sendiri', 'tahun': '2017', 'link' : 'https://youtu.be/c0p-61mLUGw'}
+    {'artis': 'Raisa', 'judul': 'Jatuh Hati', 'tahun': '2015', 'link' : 'https://youtu.be/9gY0vpjbDuA'}
+    {'artis': 'Raisa', 'judul': 'Usai Disini', 'tahun': '2018', 'link' : 'https://youtu.be/nqHFCV_3PxU'}
+    {'artis': 'Raisa', 'judul': 'LDR', 'tahun': '2014', 'link' : 'https://youtu.be/sdPKH88gqjQ'}
+    {'artis': 'Raisa', 'judul': 'Serba Salah', 'tahun': '2011', 'link' : 'https://youtu.be/NhefLOmwcdw'}
+    {'artis': 'Raisa', 'judul': 'Lagu Untukmu', 'tahun': '2018', 'link' : 'https://youtu.be/q2PR9Ci8HNA'}
+    {'artis': 'Raisa', 'judul': 'Apalah (Arti Menunggu)', 'tahun': '2012', 'link' : 'https://youtu.be/nx_kW1f5W6M'}
+    {'artis': 'Raisa', 'judul': 'Kali Kedua', 'tahun': '2017', 'link' : 'https://youtu.be/SHj2kJzVi_g'}
+    {'artis': 'Afgan', 'judul': 'Jodoh Pasti Bertemu', 'tahun': '2014', 'link' : 'https://youtu.be/MV_euZ_lKH8'}
+    {'artis': 'Afgan', 'judul': 'Bawalah Cintaku', 'tahun': '2013', 'link' : 'https://youtu.be/CPENPhizI-U'}
+    {'artis': 'Afgan', 'judul': 'Terima Kasih Cinta', 'tahun': '2013', 'link' : 'https://youtu.be/Hr0AAqHLLSg'}
+    {'artis': 'Afgan', 'judul': 'Sadis', 'tahun': '2013', 'link' : 'https://youtu.be/f45peONgdIY'}
+    {'artis': 'Afgan', 'judul': 'Kunci Hati', 'tahun': '2017', 'link' : 'https://youtu.be/JYlAdZkpWIQ'}
+    {'artis': 'Afgan', 'judul': 'Bukan CInta Biasa', 'tahun': '2013', 'link' : 'https://youtu.be/nshdkoeu_fY'}
+    {'artis': 'Judika', 'judul': 'Aku Yang Terluka', 'tahun': '2023', 'link' : 'https://youtu.be/qfdRBWYZWlc'}
+    {'artis': 'Judika', 'judul': 'Tak Mungkin Bersama', 'tahun': '2020', 'link' : 'https://youtu.be/H-GZ5wu2ufU'}
+    {'artis': 'Judika', 'judul': 'Bagaimana Kalau Aku Tidak Baik Baik Saja', 'tahun': '2022', 'link' : 'https://youtu.be/Psr7yfO_WY0'}
+    {'artis': 'Judika', 'judul': 'Bukan Dia Tapi Aku', 'tahun': '2012', 'link' : 'https://youtu.be/IfPCRfaQBA0'}
+    {'artis': 'Judika', 'judul': 'Putus Atau Terus', 'tahun': '2021', 'link' : 'https://youtu.be/47SpkXg_sYY'}
+    {'artis': 'Judika', 'judul': 'Jikalau Kau Cinta', 'tahun': '2018', 'link' : 'https://youtu.be/4Xi9hlJhfOw'}
+    {'artis': 'Judika', 'judul': 'Cinta Karena Cinta', 'tahun': '2020', 'link' : 'https://youtu.be/FZjk56FfdJY'}
+    {'artis': 'Judika', 'judul': 'Aku Yang Tersakiti', 'tahun': '2012', 'link' : 'https://youtu.be/9tZPmSv6DTs'}
+    {'artis': 'Judika', 'judul': 'Jadi Aku Sebentar Saja', 'tahun': '2017', 'link' : 'https://youtu.be/Oo9BsGPWWV0'}
+    {'artis': 'Judika', 'judul': 'Mama Papa Larang', 'tahun': '2013', 'link' : 'https://youtu.be/lY7T8Mz-18s'}
+    {'artis': 'Judika', 'judul': 'Malaikat', 'tahun': '2014', 'link' : 'https://youtu.be/p8IbIHTFppI'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Jangan Rubah Takdirku', 'tahun': '2018', 'link' : 'https://youtu.be/hPJpNXL7WXo'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Kumau Dia', 'tahun': '2020', 'link' : 'https://youtu.be/ihSQUBV_X1k'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Hanya Rindu', 'tahun': '2019', 'link' : 'https://youtu.be/CJC5PY5erzI'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Sampai Tua Nanti', 'tahun': '2021', 'link' : 'https://youtu.be/cfYhBLDWlvw'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Cinta Luar Biasa', 'tahun': '2019', 'link' : 'https://youtu.be/FcOctsNXyjk'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Senyumlah', 'tahun': '2021', 'link' : 'https://youtu.be/Cs8wtborzRU'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Bisa Tanpamu', 'tahun': '2022', 'link' : 'https://youtu.be/deaNzyAZhHw'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Tiba Tiba', 'tahun': '2021', 'link' : 'https://youtu.be/V16PvIA6k14'}
+    {'artis': 'Andmesh Kamaleng', 'judul': 'Anugerah Terindah', 'tahun': '2023', 'link' : 'https://youtu.be/o25_Tn8BnFM'}
+    {'artis': 'Bunga Citra Lestari', 'judul': 'Karena Kucinta Kau', 'tahun': '2013', 'link' : 'https://youtu.be/yYfPmWtU17U'}
+    {'artis': 'Bunga Citra Lestari', 'judul': 'Cinta Sejati', 'tahun': '2013', 'link' : 'https://youtu.be/Bgt9g1q-xj0'}
+    {'artis': 'Bunga Citra Lestari', 'judul': 'Kecewa', 'tahun': '2013', 'link' : 'https://youtu.be/173tZuk_2lY'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+    {'artis': 'Tiara Andini', 'judul': 'Janji Setia', 'tahun': '2022', 'link' : 'https://youtu.be/PVIvHCX35hU'}
+
+
+]
