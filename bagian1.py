8a14a687bc45ffc163e5cf3bc5c9de077e1f40f5
Gabeta Estefan SIbarani <gabetaestefansibarani@gmail.com>
2023-06-06 09:54:40 +0700

    kita uda pusing

--- bagian1.py
+++ bagian1.py
@@ -106,26 +106,131 @@ if pilihan_1 == "1":
                 menu_awal()
              else:
                 print("Terimakasih telah mengunjungi informasi genre lagu.")
+    
+    #POP
     elif pilihan_2 == "2":
         print("Genre musik pop adalah salah satu genre musik yang terkenal. Bahkan genre musik pop bisa dibilang sebagai aliran musik paling populer dan paling banyak dinyanyikan oleh penyanyi dan band di seluruh dunia.")
-        import List_Lagu
-        tahun_pencarian = input("Masukkan tahun yang ingin anda cari: ")
+        from List_Lagu import Pop
+        print(Pop)
+        print("1. Artis")
+        print("2. Judul")
+        print("3. Tahun")
+        print("4. Keluar")
+        pilihan_3 = input("Masukkan filter yang ingin anda pilih: ")
+        if pilihan_3 == "1":
+            pencarian_artis = input("Masukkan artis yang ingin anda cari: ")
+            for lagu in Pop:
+                if lagu['artis'] == pencarian_artis:
+                    print("Artis:", lagu['artis'])
+                    print("Judul:", lagu['judul'])
+                    print("Tahun:", lagu['tahun'])
+                else:
+                    ("Maaf lagu yang anda cari tidak tersedia")
+                    pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+                    if pilihan_4.lower() == "ya":
+                        menu_awal()
+                    else:
+                        print("Terimakasih telah mengunjungi informasi genre lagu.")
+        elif pilihan_3 == "2":
+            pencarian_judul = input("Masukkan judul yang ingin anda cari: ")
+            for lagu in Pop:
+                if lagu['judul'] == pencarian_judul:
+                    print("Artis:", lagu['artis'])
+                    print("Judul:", lagu['judul'])
+                    print("Tahun:", lagu['tahun'])
+                else:
+                    ("Maaf lagu yang anda cari tidak tersedia")
+                    pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+                    if pilihan_4.lower() == "ya":
+                        menu_awal()
+                    else:
+                        print("Terimakasih telah mengunjungi informasi genre lagu.")
+        elif pilihan_3 == "3":
+            pencarian_tahun = input("Masukkan tahun yang ingin anda cari: ")
+            for lagu in Pop:
+                if lagu['tahun'] == pencarian_tahun:
+                    print("Artis:", lagu['artis'])
+                    print("Judul:", lagu['judul'])
+                    print("Tahun:", lagu['tahun'])
+                else:
+                    ("Maaf lagu yang anda cari tidak tersedia")
+                    pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+                    if pilihan_4.lower() == "ya":
+                        menu_awal()
+                    else:
+                        print("Terimakasih telah mengunjungi informasi genre lagu.")
+        elif pilihan_3 == "4":
+            print("Terimakasih telah mengunjungi genre lagu pop.")
+        else:
+             print("Maaf pilihan yang anda masukkan tidak tersedia.")
+             pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+             if pilihan_4.lower() == "ya":
+                menu_awal()
+             else:
+                print("Terimakasih telah mengunjungi informasi genre lagu.")
 
-        for lagu in List_Lagu.Pop:
-            if lagu[''] == tahun_pencarian:
-                print("Artis:", lagu['artis'])
-                print("Judul:", lagu['judul'])
-                print("Link:", lagu['link'])
+#K-POP
     elif pilihan_2 == "3":
         print("Genre musik k-pop adalah genre musik populer yang berasal dari Korea Selatan. Lagu-lagu K-pop banyak dipengaruhi oleh berbagai genre musik, seperti hip-hop, electronic dance, jazz, dan rock.")
-        import List_Lagu
-        tahun_pencarian = input("Masukkan tahun yang ingin Anda cari: ")
-
-        for lagu in List_Lagu.Kpop:
-            if lagu['tahun'] == tahun_pencarian:
-                print("Artis:", lagu['artis'])
-                print("Judul:", lagu['judul'])
-                print("Link:", lagu['link'])
+        from List_Lagu import Kpop
+        print(Kpop)
+        print("1. Artis")
+        print("2. Judul")
+        print("3. Tahun")
+        print("4. Keluar")
+        pilihan_3 = input("Masukkan filter yang ingin anda pilih: ")
+        if pilihan_3 == "1":
+            pencarian_artis = input("Masukkan artis yang ingin anda cari: ")
+            for lagu in Kpop:
+                if lagu['artis'] == pencarian_artis:
+                    print("Artis:", lagu['artis'])
+                    print("Judul:", lagu['judul'])
+                    print("Tahun:", lagu['tahun'])
+                else:
+                    ("Maaf lagu yang anda cari tidak tersedia")
+                    pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+                    if pilihan_4.lower() == "ya":
+                        menu_awal()
+                    else:
+                        print("Terimakasih telah mengunjungi informasi genre lagu.")
+        elif pilihan_3 == "2":
+            pencarian_judul = input("Masukkan judul yang ingin anda cari: ")
+            for lagu in Kpop:
+                if lagu['judul'] == pencarian_judul:
+                    print("Artis:", lagu['artis'])
+                    print("Judul:", lagu['judul'])
+                    print("Tahun:", lagu['tahun'])
+                else:
+                    ("Maaf lagu yang anda cari tidak tersedia")
+                    pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+                    if pilihan_4.lower() == "ya":
+                        menu_awal()
+                    else:
+                        print("Terimakasih telah mengunjungi informasi genre lagu.")
+        elif pilihan_3 == "3":
+            pencarian_tahun = input("Masukkan tahun yang ingin anda cari: ")
+            for lagu in Kpop:
+                if lagu['tahun'] == pencarian_tahun:
+                    print("Artis:", lagu['artis'])
+                    print("Judul:", lagu['judul'])
+                    print("Tahun:", lagu['tahun'])
+                else:
+                    ("Maaf lagu yang anda cari tidak tersedia")
+                    pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+                    if pilihan_4.lower() == "ya":
+                        menu_awal()
+                    else:
+                        print("Terimakasih telah mengunjungi informasi genre lagu.")
+        elif pilihan_3 == "4":
+            print("Terimakasih telah mengunjungi genre lagu kpop.")
+        else:
+             print("Maaf pilihan yang anda masukkan tidak tersedia.")
+             pilihan_4 = input("Apakah anda ingin kembali ke menu awal? (Ya/Tidak) ")
+             if pilihan_4.lower() == "ya":
+                menu_awal()
+             else:
+                print("Terimakasih telah mengunjungi informasi genre lagu.")
+ 
     elif pilihan_2 == "4":
         print("Terimakasih telah mengunjungi informasi genre lagu.")
     else:
